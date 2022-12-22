b94f60b7e0cdcbbbbccd7c78241c5c8da020784e
Your Name <you@example.com>
2022-12-21 22:32:26 -0500

    function definition with smol example

---
+++ app.py
@@ -0,0 +1,11 @@
+# variables
+
+# expressions 
+
+# operations 
+
+# functions: print() write text on the screen, type()tells us the certain type of value,
+# str() converts number into a string
+
+def greeting(name):
+    print("Welcome, " + name)
