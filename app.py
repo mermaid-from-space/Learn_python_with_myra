f2e75af91fdccc8f6ba9469c84bea1a14027ff0f
mermaid-from-space <treesmellgood@gmail.com>
2022-12-30 16:50:22 -0500

    final quiz on while loops answers

--- app.py
+++ app.py
@@ -365,3 +365,93 @@ while True:
 
 # How do you avoid the most common pitfalls when writing while loops? 
 # First, remember to initialize your variables, and second, check that your loops won't run forever. 
+
+# make the print_prime_factors function print all the prime factors of a number.
+# A prime factor is a number that is prime and divides another without a remainder.
+
+def print_prime_factors(number):
+  # Start with two, which is the first prime
+  factor = 2
+  # Keep going until the factor is larger than the number
+  while factor <= number:
+    # Check if factor is a divisor of number
+    if number % factor == 0:
+      # If it is, print it and divide the original number
+      print(factor)
+      number = number / factor
+    else:
+      # If it's not, increment the factor by one
+      factor = factor + 1
+    #   could also be typed factor+=1
+  return "Done"
+
+print_prime_factors(100)
+
+
+# `````````````````````````````````````````````````````````````````````````````````````````````````
+# answer to: The following code can lead to an infinite loop. 
+# Fix the code so that it can finish successfully for all numbers.
+# Note: Try running your function with the number 0 as the input, and see what you get!
+
+def is_power_of_two(n):
+  # Check if the number can be divided by two without a remainder
+  while n % 2 == 0:
+    if n == 0:
+      break
+    n = n / 2
+  # If after dividing by two the number is 1, it's a power of two
+  if n == 1:
+    return True
+  return False
+  
+
+print(is_power_of_two(0)) # Should be False
+print(is_power_of_two(1)) # Should be True
+print(is_power_of_two(8)) # Should be True
+print(is_power_of_two(9)) # Should be False
+
+# ..................................................................
+
+# answer to: Fill in the empty function so that it returns the sum of all the divisors of a number, 
+# without including it. 
+# A divisor is a number that divides into another without a remainder.
+
+def sum_divisors(n):
+    sum = 0
+    x = 1
+    if sum == n:
+        return sum
+    while x < n:
+        if n % x == 0:
+            sum = sum + x
+        x += 1
+    # Return the sum of all divisors of n, not including n
+    return sum
+
+# ................................................
+
+# ANSWER: The multiplication_table function prints the results of a number passed to it multiplied by 1 through 5. 
+# An additional requirement is that the result is not to exceed 25, which is done with the break statement. 
+# Fill in the blanks to complete the function to satisfy these conditions.
+
+def multiplication_table(number):
+	# Initialize the starting point of the multiplication table
+	multiplier = 1
+	# Only want to loop through 5
+	while multiplier <= 5:
+		result = number * multiplier
+		# What is the additional condition to exit out of the loop?
+		if result > 25:
+			break
+		print(str(number) + "x" + str(multiplier) + "=" + str(result))
+		# Increment the variable for the loop
+		multiplier += 1
+
+multiplication_table(3) 
+# Should print: 3x1=3 3x2=6 3x3=9 3x4=12 3x5=15
+
+multiplication_table(5) 
+# Should print: 5x1=5 5x2=10 5x3=15 5x4=20 5x5=25
+
+multiplication_table(8)	
+# Should print: 8x1=8 8x2=16 8x3=24
