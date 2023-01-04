ee508cc3d8dce91eea8dcc0726dd9866c3a7382d
mermaid-from-space <treesmellgood@gmail.com>
2023-01-04 16:15:57 -0500

    practice problems from quiz

--- app.py
+++ app.py
@@ -513,3 +513,69 @@ def greet_friends(friends):
         print("Hi " + friend)
 
 greet_friends(['Taylor' , 'Luisa' , 'Jamaal' , 'Eli'])
+
+
+# MULTIPLES OF A NUMBER
+def multiples(x):
+    for n in range(100):
+        n = (n*x)
+        if n <= 100:
+            print(n)
+print(multiples(7))
+
+
+# make the factorial function return the factorial of n. Then, print the first 10 factorials (from 0 to 9) with the corresponding number.
+#  Remember that the factorial of a number is defined as the product of an integer and all integers before it. 
+# For example, the factorial of five (5!) is equal to 1*2*3*4*5=120. Also recall that the factorial of zero (0!) is equal to 1.
+
+def factorial(n):
+    result = 1
+    for x in range(2,n+1):
+        result = result * x
+    return result
+
+for n in range(10):
+    print(n, factorial(n))
+
+
+# Write a script that prints the first 10 cube numbers (x**3), starting with x=1 and ending with x=10.
+for x in range(1,11):
+  print(x**3)
+
+
+# Write a script that prints the multiples of 7 between 0 and 100. 
+# Print one multiple per line and avoid printing any numbers that aren't multiples of 7.
+#  Remember that 0 is also a multiple of 7.
+
+# def multiples(x):
+#     for n in range(100):
+#         n = (n*x)
+#         if n <=100:
+#             print(n)
+# print(multiples(7))
+
+
+for i in range(0,100):
+    if i % 7 == 0:
+        print(i)
+
+the range function is used to create a series of numbers from a given range.
+ Here the range is used to generate numbers from 0 to 100, 
+ Here "if" statement is used to distinguish between numbers divisible by 7 and not divisible by 7, 
+ and finally the number divisible by 7 is printed.
+
+# Question 5
+# The retry function tries to execute an operation that might fail, it retries the operation for a number of attempts.  
+# Currently the code will keep executing the function even if it succeeds. 
+# Fill in the blank so the code stops trying after the operation succeeded. break was the answer
+
+def retry(operation, attempts):
+  for n in range(attempts):
+    if operation():
+      print("Attempt " + str(n) + " succeeded")
+      break
+    else:
+      print("Attempt " + str(n) + " failed")
+
+retry(create_user, 3)
+retry(stop_service, 5)
