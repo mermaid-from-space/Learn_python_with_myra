ca86287e79db57a8af0ed8c2910c2a06925fc54c
mermaid-from-space <treesmellgood@gmail.com>
2023-01-12 20:53:09 -0500

    replace old domain function

--- app.py
+++ app.py
@@ -862,3 +862,14 @@ pets.index("C")
 print(pets.index("a"))
 print("Dragons" in pets) 
 print("Cats" in pets)
+
+# Imagine that your company has recently moved to using a new domain, 
+# but a lot of the company email addresses are still using the old one. 
+# You want to write a program that replaces this old domain with the new one in any outdated email addresses.
+#  The function to replace the domain would look like this.
+def replace_domain(email, old_domain, new_domain):
+  if "@" + old_domain in email:
+      index = email.index("@" + old_domain)
+      new_email = email[:index] + "@" + new_domain
+      return new_email
+  return email  
