57dcc07252f7b33ec4679725626f60ce294c8898
mermaid-from-space <treesmellgood@gmail.com>
2023-01-16 21:17:39 -0500

    tuples are kewl

--- app.py
+++ app.py
@@ -1091,4 +1091,31 @@ print(fruits)
 fruits[2] = "Strawberry"
 print(fruits)
 
+# *********TUPLES OMG********
+# Tuples are sequences of elements of any type that are immutable. We write tuples in parentheses instead of square brackets.
+# when using tuples the position of the elements inside the tuple have meaning. 
+# Tuples are used for lots of different things in Python. One common example is the return value of functions. When a function returns more than one value, it's actually returning a tuple. Remember the function to convert seconds to hours, minutes, and seconds that we saw a while back? Here just to remind you. 
+# This function returns three values. In other words, it returns a tuple of three elements.
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+
+result = convert_seconds(5000)
+print(result)
+hours, minutes, seconds = result
+print(hours, minutes, seconds)
+hours, minutes, seconds = convert_seconds(1000)
+print(hours, minutes, seconds)
+
+# Let's use tuples to store information about a file: its name, its type and its size in bytes. 
+# Fill in the gaps in this code to return the size in kilobytes (a kilobyte is 1024 bytes) up to 2 decimal places. 
+def file_size(file_info):
+	name, type, size = file_info
+	return("{:.2f}".format(size / 1024))
+
+print(file_size(('Class Assignment', 'docx', 17875))) # Should print 17.46
+print(file_size(('Notes', 'txt', 496))) # Should print 0.48
+print(file_size(('Program', 'py', 1239))) # Should print 1.21
 
