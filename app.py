d87fc8a870d25dac795da82de1b3be9db1bc912d
mermaid-from-space <treesmellgood@gmail.com>
2023-02-16 14:20:18 -0500

    format address function

--- app.py
+++ app.py
@@ -1418,3 +1418,39 @@ groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59,
 	"coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
 
 print(add_prices(groceries)) # Should print 28.44
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# mod 4 graded assessment
+def format_address(address_string):
+  # Declare variables
+  house_number = 0
+  street_name = []
+
+  # Separate the address string into parts
+  address = address_string.split()
+
+  # Traverse through the address parts
+  for item in address:
+    if item.isnumeric():
+      house_number = item
+    else:
+      street_name.append(item)
+    # Determine if the address part is the
+    # house number or part of the street name
+
+  # Does anything else need to be done 
+  # before returning the result?
+  
+  # Return the formatted string  
+  return "house number {} on street named {}".format(house_number, " ".join(street_name))
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
