e4a84f2f3a38c8609283f5b9f8856416cc903103
mermaid-from-space <treesmellgood@gmail.com>
2023-02-16 14:41:01 -0500

    combine quests and count letters function

--- app.py
+++ app.py
@@ -1480,5 +1480,58 @@ Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
 print(combine_lists(Jamies_list, Drews_list))
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# list comprehension to create list of squared numbers (n*n)
+def squares(start, end):
+	return [(x*x) for x in range(start,end+1)]
 
+print(squares(2, 3)) # Should be [4, 9]
+print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# iterate through the keys and values of the car_prices dictionary, printing out some information about each one.
+def car_listing(car_prices):
+  result = ""
+  for key, value in car_prices.items():
+    result += "{} costs {} dollars".format(key,value) + "\n"
+  return result
+
+print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# count letters
+def count_letters(text):
+  elements = text.replace(" ", "").lower()
+  result = {}
+  # Go through each letter in the text
+  for letter in elements:
+    # Check if the letter needs to be counted or not
+    if letter.isalpha():
+      if letter not in result:
+        result[letter] = 1
+      else:
+        result[letter] +=1
+    # Add or increment the value in the dictionary
+  return result
+
+print(count_letters("AaBbCc"))
+# Should be {'a': 2, 'b': 2, 'c': 2}
+
+print(count_letters("Math is fun! 2+2=4"))
+# Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+
+print(count_letters("This is a sentence."))
+# Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# combine guests!
+def combine_guests(guests1, guests2):
+  # Combine both dictionaries into one, with each key listed 
+  # only once, and the value from guests1 taking precedence
+  guests2.update(guests1)
+  return guests2
+
+Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
+Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
+
+print(combine_guests(Rorys_guests, Taylors_guests))
